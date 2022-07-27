823619de5458cc00b4e0926e0de6d457ea81dd22
Emre Kaan Satış <redbirdstwo@hotmail.com>
2022-07-28 00:58:32 +0300

    final

---
+++ README.md
@@ -0,0 +1,7 @@
+# Eliptic Curve Addition
+
+P + Q = R is the additive property defined geometrically.
+
+Elliptic curve groups are additive groups; that is, their basic function is addition. The addition of two points in an elliptic curve is defined geometrically.
+
+The negative of a point P = (xP,yP) is its reflection in the x-axis: the point -P is (xP,-yP). Notice that for each point P on an elliptic curve, the point -P is also on the curve.
--- curve.py
+++ curve.py
@@ -13,20 +13,29 @@ class ElipticCurve:
         self.b = b
         self.p = p  # mod p
 
+    def inv_mod_p(self, x):
+        if x % self.p == 0:
+            raise ZeroDivisionError("Impossible inverse")
+        return pow(x, self.p - 2, self.p)
+
     def addition(self, p1, p2):
         if p2 == Point(0, 0):
-            print("P1 + P2 = ", "(", p1.x, ",", p1.y, ")")
-        if p1 == Point(0, 0):
-            print("P1 + P2 = ", "(", p2.x, ",", p2.y, ")")
-        if p1 == p2:
-            l = (3 * p1.x**2 + self.a) / (2 * p1.y) % self.p
+            result = p1
+        elif p1 == Point(0, 0):
+            result = p2
+        elif p1 == p2:
+            l = (3 * p1.x**2 + self.a) * self.inv_mod_p(2 * p1.y)
         else:
-            l = (p2.y - p1.y) / (p2.x - p1.x) % self.p
-            pass
+            l = (p2.y - p1.y) * self.inv_mod_p(p2.x - p1.x)
+        x = (l**2 - p1.x - p2.x) % self.p
+        y = -(l * x + p1.y - l * p1.x) % self.p
+        result = Point(x, y)
+
+        return result.x, result.y
 
 
-p1 = Point(0, 0)
-p2 = Point(0, 0)
-ec = ElipticCurve(3, 8, 13)
+p1 = Point(5, 1)
+p2 = Point(5, 1)
+ec = ElipticCurve(2, 2, 17)
 
 print(ec.addition(p1, p2))
